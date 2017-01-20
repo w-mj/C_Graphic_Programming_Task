9d65d7a912af068949ae7dbbf07a5a264070fef2
WMJ <w.mingjian@yahoo.com>
2017-01-20 22:37:05 +0800

    finish

--- Task2/main.c
+++ Task2/main.c
@@ -5,7 +5,7 @@
 #include "calculator.h"
 #include "tree.h"
 #include "timer.h"
-
+const int FPS = 30;
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
@@ -52,18 +52,21 @@ int main(int argc, char *argv[])
     // 显示窗口（x1，y1）与实际坐标（x0，y0）之间的相对位置
     // x1 = x0 + X 
     // y1 = -y0 + Y
-    float k = 0.1;
+    double k = 1; // 放大率
     int X = 400, Y = 300;  
     int x1, y1;
+    double ty;
     int lastx, lasty;
     int usedTime;
     bool first = true, action = true;
     t = transTree(argv[1]);
     bool quit = false;
     int verticalSpeed = 0, horizontalSpeed = 0;
+    double zoomSpeed = 1.0;
     while (!quit) {
         startTimer(fps);
         if (action) { 
+            //fprintf(stderr, "%f\n", k);
             action = false;
             SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff); // 清屏
             SDL_RenderFillRect(renderer, &blank);
@@ -75,7 +78,8 @@ int main(int argc, char *argv[])
                 SDL_RenderDrawLine(renderer, X, 0, X, 600);
             first = true;
             for (x1 = 0; x1 <= 800; x1++) {
-                y1 = (int)(Y - calculate(t, (x1  - X))  + 0.5); // 计算出y的值
+                ty = calculate(t, (x1  - X) / k); // 计算出y的值
+                y1 = Y - (int)(ty * k + 0.5);
                 //y1 = (Y - (x1 - X)); 
                 if (first) first = false;
                 else {
@@ -94,17 +98,22 @@ int main(int argc, char *argv[])
                 quit = true;
             else if (event.type == SDL_KEYDOWN) {
                 if (event.key.keysym.sym == SDLK_DOWN) {
-                    verticalSpeed = 1;
+                    verticalSpeed = 3;
                 } else if (event.key.keysym.sym == SDLK_UP) {
-                    verticalSpeed = -1;
+                    verticalSpeed = -3;
                 } else if (event.key.keysym.sym == SDLK_LEFT) {
-                    horizontalSpeed = -1;
+                    horizontalSpeed = -3;
                 } else if (event.key.keysym.sym == SDLK_RIGHT) {
-                    horizontalSpeed = 1;
+                    horizontalSpeed = 3;
                 } else if (event.key.keysym.sym == SDLK_c) {
                     X = 400;
                     Y = 300;
+                    k = 1;
                     action = true;
+                } else if(event.key.keysym.sym == SDLK_z) {
+                    zoomSpeed = 2;
+                } else if(event.key.keysym.sym == SDLK_x) {
+                    zoomSpeed = 0.5;
                 }
             }
             else if (event.type == SDL_KEYUP) {
@@ -116,19 +125,24 @@ int main(int argc, char *argv[])
                     horizontalSpeed = 0;
                 } else if (event.key.keysym.sym == SDLK_RIGHT) {
                     horizontalSpeed = 0;
+                } else if(event.key.keysym.sym == SDLK_z) {
+                    zoomSpeed = 1;
+                } else if(event.key.keysym.sym == SDLK_x) {
+                    zoomSpeed = 1;
                 }
             }
         }
 
-        if (horizontalSpeed + verticalSpeed != 0) {
+        if (horizontalSpeed + verticalSpeed + zoomSpeed!= 0) {
             Y += verticalSpeed;
             X += horizontalSpeed;
+            k *= zoomSpeed;
             action = true;
         }
 
-        usedTime = getTimer(fps);
-        if (usedTime < 10) {
-            SDL_Delay (10 - usedTime);
+        usedTime = getTimer(fps); // fps
+        if (usedTime < 1000/FPS) {
+            SDL_Delay (1000/FPS - usedTime);
             haltTimer(fps);
         }
         SDL_RenderPresent(renderer);
